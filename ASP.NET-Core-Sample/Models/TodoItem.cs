58e8e64d12b3efcd86d25a71df8473bc4b9e2060
すー <aaaiiiuuu35@gmail.com>
2019-05-20 19:20:00 +0900

    Create TodoItem.cs

---
+++ ASP.NET-Core-Sample/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP.NET_Core_Sample.Models
+{
+    public class TodoItem
+    {
+        public Int64 Id { get; set; }
+        public String Name { get; set; }
+        public Boolean IsComplete { get; set; }
+    }
+}
